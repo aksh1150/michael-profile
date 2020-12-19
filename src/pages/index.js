f26d1757c8e371df6c9fc86f6000ba6d7334343b
Akshit Patel <aksh1150@gmail.com>
2020-12-19 11:49:22 -0500

    Add link

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
 
 const Home = () => {
-  return <div>Michael Profile</div>
+  return (
+    <div>
+      <h1>Hello</h1>
+      <p>
+        <Link to="/uxCaseStudy">UX Case Studies</Link>
+      </p>
+    </div>
+  )
 }
 export default Home
