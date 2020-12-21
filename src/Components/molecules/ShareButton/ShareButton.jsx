88bbd50ffe6e036624cdc8f8510d142ffc193d1e
Akshit Patel <aksh1150@gmail.com>
2020-12-21 17:28:21 -0500

    All updated

--- src/Components/molecules/ShareButton/ShareButton.jsx
+++ src/Components/molecules/ShareButton/ShareButton.jsx
@@ -1,7 +1,6 @@
 import React from "react"
 import "./ShareButton.scss"
 import { BaseLink } from "../../atoms"
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 
 const ShareButton = ({ iconData }) => {
   const btn = (
