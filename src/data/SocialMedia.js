5c5ef1d4fa70ccf3fa62c8c49baab43a1cc95530
Akshit Patel <aksh1150@gmail.com>
2020-12-22 13:53:31 -0500

    Create data dir to store site data

---
+++ src/data/SocialMedia.js
@@ -0,0 +1,16 @@
+const SocialMediaData = [
+  {
+    link: "https://abc.com",
+    icon: "email_icon",
+  },
+  {
+    link: "https://facebook.com",
+    icon: "FB_icon",
+  },
+  {
+    link: "https://linkedin.com",
+    icon: "linkedin_icon",
+  },
+]
+
+export default SocialMediaData
