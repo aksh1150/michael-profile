5e25acaf7cc39579b2358bbba6aa947d58f5a9e4
Akshit Patel <aksh1150@gmail.com>
2020-12-23 12:19:17 -0500

    Add fonts

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react"
 import { Link } from "gatsby"
 import "bootstrap/dist/css/bootstrap.min.css"
 import { Footer, Button, ShareButton, Slider } from "../Components/molecules"
+import { Heading, Paragraph } from "../Components/atoms"
 import { BaseLink } from "../Components/atoms"
 
 import SocialMediaData from "../data/SocialMedia"
@@ -10,7 +11,8 @@ import SliderData from "../data/Slider"
 const Home = () => {
   return (
     <div>
-      <h1>Hello</h1>
+      <Heading type="h1">Hello</Heading>
+      <Paragraph size="lg">Thi si stext Paragraph</Paragraph>
       <Link to="/uxCaseStudy">UX Case Studies</Link>
       <Button link="/uxCaseStudy" internal size="lg">
         Large button
