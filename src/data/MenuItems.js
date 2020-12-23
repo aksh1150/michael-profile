916c2dedea92597e6a5daa9ccc3714423471f46d
Akshit Patel <aksh1150@gmail.com>
2020-12-23 14:13:07 -0500

    Create an object for Menu otems

---
+++ src/data/MenuItems.js
@@ -0,0 +1,16 @@
+const Menu = [
+  {
+    title: "UX Case Studies",
+    link: "uxCaseStudy",
+  },
+  {
+    title: "Graphics",
+    link: "graphics",
+  },
+  {
+    title: "About",
+    link: "about",
+  },
+]
+
+export default Menu
