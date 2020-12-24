2a0c6c0d340e4585b1a7053640c1825dfd9f5f22
Akshit Patel <aksh1150@gmail.com>
2020-12-24 11:44:01 -0500

    Complete first section

--- src/pages/index.js
+++ src/pages/index.js
@@ -59,41 +59,15 @@ const Home = () => {
             <Col xs={12} sm={1} className="text-center">
               <ScrollDown />
             </Col>
-            <Col xs={12} sm={5} className="text-center">
-              <Button link="/uxCaseStudy" internal size="lg">
+            <Col xs={12} sm={5} className="text-center marginTop">
+              <Button link="/uxCaseStudy" internal>
                 UX Case Studies
               </Button>
             </Col>
           </Row>
         </Container>
-        {/* <Heading type="h1">Hello</Heading>
-        <Paragraph size="lg">Thi si stext Paragraph</Paragraph>
-        <ShareButton iconData={SocialMediaData} />
-        <Slider slideData={SliderData} /> */}
       </Section>
     </Layout>
-    // <div>
-    //   <Header />
-    //   <div className="text-center">
-    //     <ScrollDown />
-    //   </div>
-    //   <Heading type="h1">Hello</Heading>
-    //   <Paragraph size="lg">Thi si stext Paragraph</Paragraph>
-    //   <Section background="Grey" />
-    //   <BaseLink link="/uxCaseStudy" internal>
-    //     UX Case Studies
-    //   </BaseLink>
-    //   <Button link="/uxCaseStudy" internal size="lg">
-    //     Large button
-    //   </Button>
-    //   <br />
-    //   <Button link="/uxCaseStudy" internal>
-    //     Small button
-    //   </Button>{" "}
-    //   <ShareButton iconData={SocialMediaData} />
-    //   <Slider slideData={SliderData} />
-    //   <Footer />
-    // </div>
   )
 }
 export default Home
