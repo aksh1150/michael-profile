4aedd147047ab5fde6a65a8a71d7fcecb856ad46
Akshit Patel <aksh1150@gmail.com>
2021-01-10 15:10:33 -0500

    Complete link button

--- src/Components/molecules/LinkButton/LinkButton.jsx
+++ src/Components/molecules/LinkButton/LinkButton.jsx
@@ -2,11 +2,12 @@ import React from "react"
 import { BaseLink } from "../../atoms"
 import "./LinkButton.scss"
 
-const LinkButton = ({ children, size, ...props }) => {
+const LinkButton = ({ children, size, active, ...props }) => {
   const btnSize = size === "lg" ? "large" : "small"
+  const isActive = active ? "active" : ""
   const btn = (
     <BaseLink {...props}>
-      <button type="button" className={`btns ${btnSize}`}>
+      <button type="button" className={`Linkbtns ${btnSize} ${isActive}`}>
         {children}
       </button>
     </BaseLink>
