65c6f0c581cc819016b00caa2d6bfc7921254293
Akshit Patel <aksh1150@gmail.com>
2021-01-10 15:39:45 -0500

    Fix all typography

--- src/pages/uxCaseStudy/index.js
+++ src/pages/uxCaseStudy/index.js
@@ -1,12 +1,21 @@
 import React from "react"
 import { LinkButton } from "../../Components/molecules"
-import { BaseLink } from "../../Components/atoms"
+import { BaseLink, Heading } from "../../Components/atoms"
 
 const CaseStudies = () => {
   return (
     <div>
       <LinkButton>Text</LinkButton>
       <BaseLink InnerLink>Hyperlink</BaseLink>
+      <Heading type="h1" inner colour="spaceship">
+        Section
+      </Heading>
+      <Heading type="h2" inner colour="primary">
+        H2
+      </Heading>
+      <Heading type="h3" inner colour="dark">
+        H3 content
+      </Heading>
     </div>
   )
 }
