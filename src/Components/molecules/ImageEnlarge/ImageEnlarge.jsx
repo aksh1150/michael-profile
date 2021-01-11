97298b43bff1979b127d867c3c68a58810509eae
Akshit Patel <aksh1150@gmail.com>
2021-01-11 16:13:29 -0500

    Add contetnt in image enlarge component

--- src/Components/molecules/ImageEnlarge/ImageEnlarge.jsx
+++ src/Components/molecules/ImageEnlarge/ImageEnlarge.jsx
@@ -1,7 +1,18 @@
-import React from "react"
+import React, { useState } from "react"
+import Image from "react-image-enlarger"
 
-const ImageEnlarge = () => {
-  //
+const ImageEnlarge = ({ src, alt }) => {
+  const [zoomed, setZoomed] = useState(false)
+  return (
+    <Image
+      style={{ width: "auto", height: "auto" }}
+      zoomed={zoomed}
+      src={src}
+      onClick={() => setZoomed(true)}
+      onRequestClose={() => setZoomed(false)}
+      alt={alt}
+    />
+  )
 }
 
 export default ImageEnlarge
