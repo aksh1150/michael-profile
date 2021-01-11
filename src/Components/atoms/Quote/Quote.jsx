950d5ad1ec39bc54d0a96b7d68a7b5ad449fd022
Akshit Patel <aksh1150@gmail.com>
2021-01-11 15:23:54 -0500

    Add proptype

--- src/Components/atoms/Quote/Quote.jsx
+++ src/Components/atoms/Quote/Quote.jsx
@@ -1,7 +1,11 @@
 import React from "react"
+import PropTypes from "prop-types"
 
+import "./Quote.scss"
 const Quote = () => {
   //
 }
-
+Quote.propTypes = {
+  children: PropTypes.node,
+}
 export default Quote
