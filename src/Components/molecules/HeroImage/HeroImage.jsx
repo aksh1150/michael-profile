0efe8e4329b73e50d9a1aa1a17061e490e6aaab5
Akshit Patel <aksh1150@gmail.com>
2021-01-11 15:58:11 -0500

    Create Hero Image comaponent

---
+++ src/Components/molecules/HeroImage/HeroImage.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+const HeroImage = () => {
+  //
+}
+
+export default HeroImage
--- src/Components/molecules/index.js
+++ src/Components/molecules/index.js
@@ -5,5 +5,15 @@ import SocialMedia from "./SocialMedia"
 import Slider from "./Slider"
 import Header from "./Header"
 import LinkButton from "./LinkButton"
+import HeroImage from "./HeroImage"
 
-export { Footer, Button, ShareButton, SocialMedia, Slider, Header, LinkButton }
+export {
+  Footer,
+  Button,
+  ShareButton,
+  SocialMedia,
+  Slider,
+  Header,
+  LinkButton,
+  HeroImage,
+}
