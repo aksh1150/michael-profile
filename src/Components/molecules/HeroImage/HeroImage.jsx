9604d62d85aee1069a1c1d5dd22ed5e74ad6165b
Akshit Patel <aksh1150@gmail.com>
2021-01-11 16:03:33 -0500

    Add proptype

--- src/Components/molecules/HeroImage/HeroImage.jsx
+++ src/Components/molecules/HeroImage/HeroImage.jsx
@@ -1,7 +1,11 @@
 import React from "react"
+import PropTypes from "prop-types"
+import "./HeroImage.scss"
 
 const HeroImage = ({ imageURL }) => {
   return <img src={require(imageURL)} alt="Hero Image" className="HeroImage" />
 }
-
+HeroImage.propTypes = {
+  imageURL: PropTypes.string,
+}
 export default HeroImage
