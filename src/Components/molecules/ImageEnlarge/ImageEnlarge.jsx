1022ef226f8f5db1c0919a5dde841ae75996306b
Akshit Patel <aksh1150@gmail.com>
2021-01-14 17:04:40 -0500

    Add new package

--- src/Components/molecules/ImageEnlarge/ImageEnlarge.jsx
+++ src/Components/molecules/ImageEnlarge/ImageEnlarge.jsx
@@ -1,20 +1,29 @@
 import React, { useState } from "react"
 import Image from "react-image-enlarger"
 import PropTypes from "prop-types"
+import ModalImage from "react-modal-image"
 
 import "./ImageEnlarge.scss"
 
 const ImageEnlarge = ({ src, alt }) => {
   const [zoomed, setZoomed] = useState(false)
   return (
-    <Image
-      style={{ width: "auto", height: "auto" }}
-      zoomed={zoomed}
-      src={src}
-      onClick={() => setZoomed(true)}
-      onRequestClose={() => setZoomed(false)}
+    <ModalImage
+      small={src}
+      large={src}
       alt={alt}
+      className="ImageLarger"
+      hideDownload
+      imageBackgroundColor="white"
     />
+    // <Image
+    //   style={{ width: "auto", height: "auto" }}
+    //   zoomed={zoomed}
+    //   src={src}
+    //   onClick={() => setZoomed(true)}
+    //   onRequestClose={() => setZoomed(false)}
+    //   alt={alt}
+    // />
   )
 }
 
