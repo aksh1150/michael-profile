7cd797af2d8fbc3e0035bfc23cc2a096150102ed
Akshit Patel <aksh1150@gmail.com>
2021-01-15 19:14:53 -0500

    All updated

--- src/Components/molecules/ImageEnlarge/ImageEnlarge.jsx
+++ src/Components/molecules/ImageEnlarge/ImageEnlarge.jsx
@@ -4,13 +4,13 @@ import ModalImage from "react-modal-image"
 
 import "./ImageEnlarge.scss"
 
-const ImageEnlarge = ({ src, alt }) => {
+const ImageEnlarge = ({ src, alt, className }) => {
   return (
     <ModalImage
       small={src}
       medium={src}
       alt={alt}
-      className="ImageLarger"
+      className={`ImageLarger ${className}`}
       hideDownload
       imageBackgroundColor="white"
       hideZoom
