11b2358868cf61050c830ad000afd5d3160f1b8d
Akshit Patel <aksh1150@gmail.com>
2021-01-17 10:57:18 -0500

    All update

--- src/pages/uxCaseStudy/index.js
+++ src/pages/uxCaseStudy/index.js
@@ -1213,8 +1213,8 @@ const CaseStudies = () => {
                 src="//invis.io/53XRYLBXWM8"
                 frameborder="0"
                 allowfullscreen
-                data-embed="true"
                 tabIndex="0"
+                data-embed="true"
               ></iframe>
               <BulletSection italic className="text-center">
                 Test out the invision prototype
