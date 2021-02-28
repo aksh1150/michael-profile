f2d70163ad294d731c29e8efd1b8e04dfbfbc360
Akshit Patel <aksh1150@gmail.com>
2021-02-28 12:32:45 -0500

    Update btn style

--- src/Components/molecules/Header/Header.jsx
+++ src/Components/molecules/Header/Header.jsx
@@ -10,37 +10,52 @@ import "./Header.scss"
 const Header = () => {
   const [isToggle, setIsToggle] = useState(false)
 
-  const toggled = () => {
-    setIsToggle(!isToggle)
-    // const el = document.querySelector("#responsive-navbar-nav")
+  // const toggled = () => {
+  //   setIsToggle(!isToggle)
+  //   // const el = document.querySelector("#responsive-navbar-nav")
 
-    // el.classList.add("show")
-  }
+  //   // el.classList.add("show")
+  //   const el = document.querySelector("#responsive-navbar-nav")
 
-  const toggledHide = () => {
-    setIsToggle(!isToggle)
-    const el = document.querySelector("#responsive-navbar-nav")
-    if (el.classList.contains("show")) {
-      el.classList.remove("show")
+  //   el.classList.add("show")
+  // }
+
+  // const toggledHide = () => {
+  //   setIsToggle(!isToggle)
+
+  //   const el = document.querySelector("#responsive-navbar-nav")
+  //   if (el.classList.contains("show")) {
+  //     el.classList.remove("show")
+  //   }
+  // }
+
+  const handleCollapse = () => {
+    const el = document.querySelector(".navbar .navbar-toggler")
+
+    if (el.classList.contains("collapsed")) {
+      el.style.color = "white"
+      el.style.left = "auto"
+      el.style.right = "25px"
+      el.innerHTML = "X"
+      return
     }
+    el.innerHTML = '<span class="navbar-toggler-icon"></span>'
+    el.style.left = "7%"
+    el.style.right = "auto"
   }
+
   return (
     <div className="newNav">
       <Navbar collapseOnSelect expand="sm" variant="dark">
         <Navbar.Brand href="/" className="d-sm-block d-none">
           <Logo />
         </Navbar.Brand>
-        {/* <Navbar.Toggle aria-controls="responsive-navbar-nav" /> */}
-        {isToggle ? (
-          <BaseLink className="btn-outline-dark" clickEvent={toggledHide}>
-            X
-          </BaseLink>
-        ) : (
-          <Navbar.Toggle
-            aria-controls="responsive-navbar-nav"
-            onClick={toggled}
-          />
-        )}
+
+        <Navbar.Toggle
+          aria-controls="responsive-navbar-nav"
+          onClick={handleCollapse}
+        />
+
         <Navbar.Collapse
           className="justify-content-end"
           id="responsive-navbar-nav"
